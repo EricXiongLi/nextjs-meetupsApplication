f385bee11c4baf96531ac01d246c554a069c578f
EricXiongLi <xiong.li@uzh.ch>
2022-02-12 19:23:50 +0100

    improved ui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4590eb60221bec19bc21";
+/******/ 	var hotCurrentHash = "2110c4579d44f7ab29be";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
